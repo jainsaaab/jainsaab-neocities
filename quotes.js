3a2db43a2ca449be4a0964269a997f64a215b3e0
Aditya Jain <jainsaab@protonmail.com>
2021-10-10 18:11:02 +0530

    added more quotes

--- quotes.js
+++ quotes.js
@@ -1,37 +1,281 @@
-quotes = {
-    quotes: [
-        { text: "Waste no more time arguing what a good man should be. Be One.", author: "Marcus Aurelius" },
-        { text: "Think of the life you have lived until now as over and, as a dead man, see what’s left as a bonus and live it according to Nature. Love the hand that fate deals you and play it as your own, for what could be more fitting?", author: "Marcus Aurelius" },
-        { text: "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own.", author: "Marcus Aurelius" },
-        { text: "In your actions, don’t procrastinate. In your conversations, don’t confuse. In your thoughts, don’t wander. In your soul, don’t be passive or aggressive. In your life, don’t be all about business.", author: "Marcus Aurelius" },
-        { text: "If it is not right, do not do it, if it is not true, do not say it.", author: "Marcus Aurelius" },
-        { text: "The best revenge is not to be like your enemy.", author: "Marcus Aurelius" },
-        { text: "Choose not to be harmed — and you won’t feel harmed. Don’t feel harmed — and you haven’t been.", author: "Marcus Aurelius" },
-        { text: "It’s time you realized that you have something in you more powerful and miraculous than the things that affect you and make you dance like a puppet.", author: "Marcus Aurelius" },
-        { text: "External thinks are not the problem. It’s your assessment of them. Which you can erase right now.", author: "Marcus Aurelius" },
-        { text: "If anyone can refute me—show me I’m making a mistake or looking at things from the wrong perspective—I’ll gladly change. It’s the truth I’m after, and the truth never harmed anyone.", author: "Marcus Aurelius" },
-        { text: "You could leave life right now. Let that determine what you do and say and think.", author: "Marcus Aurelius" },
-        { text: "Be tolerant with others and strict with yourself.", author: "Marcus Aurelius" },
-        { text: "We are more often frightened than hurt; and we suffer more in imagination than in reality.", author: "Seneca" },
-        { text: "If a man knows not which port he sails, no wind is favorable.", author: "Seneca" },
-        { text: "No person has the power to have everything they want, but it is in their power not to want what they don’t have, and to cheerfully put to good use what they do have.", author: "Seneca" },
-        { text: "Nothing, to my way of thinking, is a better proof of a well ordered mind than a man’s ability to stop just where he is and pass some time in his own company.", author: "Seneca" },
-        { text: "He who fears death will never do anything worth of a man who is alive.", author: "Seneca" },
-        { text: "This is our big mistake: to think we look forward to death. Most of death is already gone. Whatever time has passed is owned by death.", author: "Seneca" },
-        { text: "Life is very short and anxious for those who forget the past, neglect the present, and fear the future.", author: "Seneca" },
-        { text: "I judge you unfortunate because you have never lived through misfortune. You have passed through life without an opponent—no one can ever know what you are capable of, not even you.", author: "Seneca" },
-        { text: "How does it help…to make troubles heavier by bemoaning them?", author: "Seneca" },
-        { text: "People are frugal in guarding their personal property; but as soon as it comes to squandering tim ethey are most wasteful of the one thing in which it is right to be stingy.", author: "Seneca" },
-        { text: "How long are you going to wait before you demand the best for yourself?", author: "Epictetus" },
-        { text: "Don’t seek for everything to happen as you wish it would, but rather wish that everything happens as it actually will—then your life will flow well.", author: "Epictetus" },
-        { text: "First say to yourself what you would be; and then do what you have to do.", author: "Epictetus" },
-        { text: "Curb your desire—don’t set your heart on so many things and you will get what you need.", author: "Epictetus" },
-        { text: "That’s why the philosophers warn us not to be satisfied with mere learning, but to add practice and then training. For as time passes we forget what we learned and end up doing the opposite, and hold opinions the opposite of what we should.", author: "Epictetus" },
-        { text: "Don’t explain your philosophy. Embody it.", authod: "Epictetus" },
-        { text: "The chief task in life is simply this: to identify and separate matters so that I can say clearly to myself which are externals not under my control, and which have to do with the choices I actually control. Where then do I look for good and evil? Not to uncontrollable externals, but within myself to the choices that are my own…", author: "Epictetus" },
-        { text: "If anyone tells you that a certain person speaks ill of you, do not make excuses about what is said of you but answer, ‘He was ignorant of my other faults, else he would have not mentioned these alone.'", author: "Epictetus" },
-        { text: "I begin to speak only when I’m certain what I’ll say isn’t better left unsaid.", author: "Cato" },
-        { text: "What man actually needs is not a tensionless state but rather the striving and struggling for some goal worthy of him.", author: "Viktor Frankl" },
-        { text: "When we are no longer able to change a situation, we are challenged to change ourselves.", author: "Viktor Frankl" }
+const quotes = {
+    quotes: [{
+            text: "Waste no more time arguing what a good man should be. Be One.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "Think of the life you have lived until now as over and, as a dead man, see what’s left as a bonus and live it according to Nature. Love the hand that fate deals you and play it as your own, for what could be more fitting?",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "In your actions, don’t procrastinate. In your conversations, don’t confuse. In your thoughts, don’t wander. In your soul, don’t be passive or aggressive. In your life, don’t be all about business.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "If it is not right, do not do it, if it is not true, do not say it.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "The best revenge is not to be like your enemy.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "Choose not to be harmed — and you won’t feel harmed. Don’t feel harmed — and you haven’t been.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "It’s time you realized that you have something in you more powerful and miraculous than the things that affect you and make you dance like a puppet.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "External thinks are not the problem. It’s your assessment of them. Which you can erase right now.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "If anyone can refute me—show me I’m making a mistake or looking at things from the wrong perspective—I’ll gladly change. It’s the truth I’m after, and the truth never harmed anyone.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "You could leave life right now. Let that determine what you do and say and think.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "Be tolerant with others and strict with yourself.",
+            author: "Marcus Aurelius"
+        },
+        {
+            text: "We are more often frightened than hurt; and we suffer more in imagination than in reality.",
+            author: "Seneca"
+        },
+        {
+            text: "If a man knows not which port he sails, no wind is favorable.",
+            author: "Seneca"
+        },
+        {
+            text: "No person has the power to have everything they want, but it is in their power not to want what they don’t have, and to cheerfully put to good use what they do have.",
+            author: "Seneca"
+        },
+        {
+            text: "Nothing, to my way of thinking, is a better proof of a well ordered mind than a man’s ability to stop just where he is and pass some time in his own company.",
+            author: "Seneca"
+        },
+        {
+            text: "He who fears death will never do anything worth of a man who is alive.",
+            author: "Seneca"
+        },
+        {
+            text: "This is our big mistake: to think we look forward to death. Most of death is already gone. Whatever time has passed is owned by death.",
+            author: "Seneca"
+        },
+        {
+            text: "Life is very short and anxious for those who forget the past, neglect the present, and fear the future.",
+            author: "Seneca"
+        },
+        {
+            text: "I judge you unfortunate because you have never lived through misfortune. You have passed through life without an opponent—no one can ever know what you are capable of, not even you.",
+            author: "Seneca"
+        },
+        {
+            text: "How does it help…to make troubles heavier by bemoaning them?",
+            author: "Seneca"
+        },
+        {
+            text: "People are frugal in guarding their personal property; but as soon as it comes to squandering time they are most wasteful of the one thing in which it is right to be stingy.",
+            author: "Seneca"
+        },
+        {
+            text: "How long are you going to wait before you demand the best for yourself?",
+            author: "Epictetus"
+        },
+        {
+            text: "Don’t seek for everything to happen as you wish it would, but rather wish that everything happens as it actually will—then your life will flow well.",
+            author: "Epictetus"
+        },
+        {
+            text: "First say to yourself what you would be; and then do what you have to do.",
+            author: "Epictetus"
+        },
+        {
+            text: "Curb your desire—don’t set your heart on so many things and you will get what you need.",
+            author: "Epictetus"
+        },
+        {
+            text: "That’s why the philosophers warn us not to be satisfied with mere learning, but to add practice and then training. For as time passes we forget what we learned and end up doing the opposite, and hold opinions the opposite of what we should.",
+            author: "Epictetus"
+        },
+        {
+            text: "Don’t explain your philosophy. Embody it.",
+            authod: "Epictetus"
+        },
+        {
+            text: "The chief task in life is simply this: to identify and separate matters so that I can say clearly to myself which are externals not under my control, and which have to do with the choices I actually control. Where then do I look for good and evil? Not to uncontrollable externals, but within myself to the choices that are my own…",
+            author: "Epictetus"
+        },
+        {
+            text: "If anyone tells you that a certain person speaks ill of you, do not make excuses about what is said of you but answer, ‘He was ignorant of my other faults, else he would have not mentioned these alone.'",
+            author: "Epictetus"
+        },
+        {
+            text: "I begin to speak only when I’m certain what I’ll say isn’t better left unsaid.",
+            author: "Cato"
+        },
+        {
+            text: "What man actually needs is not a tensionless state but rather the striving and struggling for some goal worthy of him.",
+            author: "Viktor Frankl"
+        },
+        {
+            text: "When we are no longer able to change a situation, we are challenged to change ourselves.",
+            author: "Viktor Frankl"
+        },
+        {
+            text: "A person should not be too honest. Straight trees are cut first and honest people are screwed first.",
+            author: "Chanakya"
+        },
+
+        {
+            text: "As soon as the fear approaches near, attack and destroy it.",
+            author: "Chanakya",
+            tags: ["fears", "inspirational"]
+        },
+
+        {
+            text: "We should not fret for what is past, nor should we be anxious about the future; men of discernment deal only with the present moment",
+            author: "Chanakya"
+        },
+
+        {
+            text: "Books are as useful to a stupid person as a mirror is useful to a blind person.",
+            author: "Chanakya, Chanakya niti",
+            tags: ["blind", "books", "mirror", "stupid"]
+        },
+
+        {
+            text: "A man is born alone and dies alone, and he experiences the good and bad consequences of his karma alone, and he goes alone to hell or the Supreme abode.",
+            author: "Chanakya, Chanakya Neeti - Gujarati Translation"
+        },
+
+        {
+            text: "Do not reveal what you have thought upon doing, but by wise council keep it secret being determined to carry it into execution.",
+            author: "Chanakya, Chanakya Neeti - Gujarati Translation"
+        },
+
+        {
+            text: "Never make friends with people who are above or below you in status. Such friendships will never give you any happiness.",
+            author: "chanakya",
+            tags: ["friendships"]
+        },
+
+        {
+            text: "The life of an uneducated man is as useless as the tail of a dog which neither covers it’s rear end, nor protects it from the bites of insects.",
+            author: "Chanakya",
+            tags: ["life", "uneducated", "useless"]
+        },
+
+        {
+            text: "That man who is without religion and mercy should be rejected. A guru without spiritual knowledge should be rejected. The wife with an offensive face should be given up and so should relatives who are without affection.",
+            author: "Chanakya",
+            tags: ["chanakya", "chanakya-neeti", "decisions", "man", "quote", "quotes-of-life", "reject", "religion", "smart", "woman"]
+        },
+
+        {
+            text: "It is better to die than to preserve this life by incurring disgrace. The loss of life causes but a moment's grief, but disgrace brings grief every day of one's life.",
+            author: "Chanakya"
+        },
+
+        {
+            text: "If one has a good disposition, what other virtue is needed? If a man has fame, what is the value of other ornamentation?",
+            author: "Chanakya, Chanakya Niti: Philosophy of Chanakya"
+        },
+
+        {
+            text: "There is poison in the fang of the serpent, in the mouth of the fly and in the sting of a scorpion; but the wicked man is saturated with it.",
+            author: "Chanakya, Chanakya Neeti, Malayalam Translation"
+        },
+
+        {
+            text: "God is not present in idols. Your feelings are your god. The soul is your temple.",
+            author: "Chanakya, Chanakya Niti: Philosophy of Chanakya"
+        },
+
+        {
+            text: "He who lives in our mind is near though he may actually be far away; but he who is not in our heart is far though he may really be nearby.",
+            author: "Chanakya, Chanakya Niti: Philosophy of Chanakya"
+        },
+
+        {
+            text: "Either Scholarship or Death will be my refuge. पाण्डित्यं शरणं वा मे मृत्युर्वा॥",
+            author: "Chanakya, Chanakya niti"
+        },
+
+        {
+            text: "There is no enemy like infatuation and fire like wrath.",
+            author: "Chanakya"
+        },
+
+        {
+            text: "Before you start some work, always ask yourself three questions - Why am I doing it, What the results might be and Will I be successful. Only when you think deeply and find satisfactory answers to these questions, go ahead.",
+            author: "Chanakya, The Arthashastra",
+            tags: ["vishnu-gupta"]
+        },
+
+        {
+            text: "Education is the best friend. An educated person is respected everywhere.",
+            author: "Chanakya"
+        },
+
+        {
+            text: "A good wife is one who serves her husband in the morning like a mother does, loves him in the day like a sister does and pleases him like a prostitute in the night.",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+
+        {
+            text: "बुद्धिमान व्यक्ति के लिए आवश्यक है कि वह अपने शत्रु पक्ष को सदा किसी-न-किसी कष्ट में उलझाए रखे। इससे वे कभी भी उसके लिए मुसीबत या संकट उत्पन्न नहीं कर पाएँगे।",
+            author: "Acharya Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "The fragrance of flowers spreads only in the direction of the wind. But the goodness of a person spreads in all directions.",
+            author: "Chanakya"
+        },
+
+        {
+            text: "The serpent, the king, the tiger, the stinging wasp, the small child, the dog owned by other people, and the fool: these seven ought not to be awakened from sleep.",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "शंख (शंखचूड़) और लक्ष्मी-दोनों ही रत्नाकर सागर की संतान माने जाते हैं। लेकिन इसमें से एक धन-वैभव से युक्त होकर संसार में पूजनीय है तो दूसरा साधु-संतों के साथ-साथ भिक्षाटन के लिए भटकता रहता है।",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "जब तक वसंत ऋतु का आगमन नहीं होता, तब तक कोयल मौन रहती है किंतु वसंत के आगमन के साथ ही वह अपनी मधुर वाणी से दसों दिशाओं को गुंजायमान करने लगती है। इस कथन द्वारा चाणक्य ने विद्वानों को अत्यंत गूढ़ परामर्श दिया है। वे कहते हैं कि उचित समय पर ही बुद्धिमान व्यक्ति को उसके अनुकूल कार्य करने चाहिए।",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "अग्नि, गुरु, ब्राह्मण, गाय, कन्या, वृद्ध तथा बच्चा-चाणक्य ने इन्हें आदर और सम्मान का भागी कहा है। इस संदर्भ में वे कहते हैं कि उपर्युक्त सभी प्राणी आदरणीय लोगों की श्रेणी में आते हैं। इसलिए मनुष्य को कभी भी इन्हें पैरों से स्पर्श नहीं करना चाहिए। ऐसा करनेवाला बुद्धिहीन मनुष्य अपार दुःख भोगता है।",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "चाणक्य के मतानुसार, दुष्ट और दुर्जन व्यक्तियों को धन की महत्त्वाकांक्षा होती है। उसे प्राप्त करने के लिए वे नीच कार्य करने से भी पीछे नहीं हटते। धन-प्राप्ति ही उनका एकमात्र ध्येय होता है। यद्यपि मध्यम वर्ग के व्यक्ति भी धन को महत्त्व देते हैं, तथापि उनके लिए मान-सम्मान भी अत्यंत महत्त्वपूर्ण होता है। इसी कारण धन की लालसा होते हुए भी वे नीच कार्य करने से डरते हैं। इसके विपरीत, सज्जन व्यक्तियों के लिए मान-सम्मान सबसे बढ़कर होता है। इसके लिए वे बड़ी-से-बड़ी बहुमूल्य वस्तु भी अस्वीकार कर देते हैं।",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "यदि हीरे को पैरों से लटकाकर काँच को सिर पर सजा लिया जाए तो भी हीरे का मूल्य कम नहीं होता। अर्थात् विद्वान् को निम्न स्थान देकर मूर्ख को उच्च स्थान पर बैठाने के बाद भी विद्वान् का महत्त्व कम नहीं होता।",
+            author: "Chanakya, Chanakya Neeti"
+        },
+
+        {
+            text: "કામવાસના સમાન કોઈ રોગ નથી. મોહથી મોટો કોઈ શત્રુ નથી. ક્રોધ જેવી કોઈ આગ નથી અને જ્ઞાન જેવું બીજું કોઈ સુખ નથી.",
+            author: "Chanakya, Sampurna Chanakya Niti"
+        }
     ]
 }
